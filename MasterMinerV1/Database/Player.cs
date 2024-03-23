036d0d1d9328f73fce6cfa7ba1d4aa7d18ff3f32
Julian Pankratz <julian.m.pankratz@gmail.com>
2024-03-23 23:55:15 +0100

    01

---
+++ MasterMinerV1/Database/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMinerV1.Database
+{
+    internal class Player
+    {
+        public string Name { get; set; } = null!;
+        public int Ores { get; set; } = 0;
+        public int ClickVal { get; set; } = 1;
+        public int TotClicks { get; set; } = 0;
+        public ICollection<Upgrade> Upgrades { get; set; }
+    }
+}
---
+++ MasterMinerV1/Database/Upgrade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMinerV1.Database
+{
+    internal class Upgrade
+    {
+        public string Name { get; set; } = null!;
+        public int Cost { get; set; } = 0!;
+        public int ClickVal { get; set; } = 0!;
+    }
+}
